386f7cce231d08fe4572662a582d530dc5c618d2
marktodman <drmtodman@gmail.com>
2023-01-19 18:05:00 +0000

    feat: Add admin panel page

--- events/urls.py
+++ events/urls.py
@@ -4,4 +4,5 @@ from django.urls import path
 
 urlpatterns = [
     path('', views.EventList.as_view(), name='events'),
+    path('admin_panel/', views.admin_panel, name='admin-panel'),
 ]
--- events/views.py
+++ events/views.py
@@ -9,3 +9,24 @@ class EventList(ListView):
     model = Event
     queryset = Event.objects.filter(published_status=1).order_by('date')
     template_name = 'events/events.html'
+
+
+# Admin Panel
+def admin_panel(request):
+    """Superuser can view all events on the database from the frontend"""
+
+    # This page can only be accessed by a superuser
+    if request.user.is_superuser:
+        events = Event.objects.all()
+
+        context = {
+                'events': events,
+                }
+
+        return render(request, 'events/admin_panel.html', context)
+
+    # For non-superusers trying to access the page
+    else:
+        messages.success(request, (
+            'Access denied. Please sign in as an admin.'))
+        return redirect('home')
