d0903ed083642e36cf34cff06d53a799ed14538a
marktodman <drmtodman@gmail.com>
2023-01-21 17:58:15 +0000

    feat: Add the add to bag functionality

--- bag/urls.py
+++ bag/urls.py
@@ -4,4 +4,5 @@ from django.urls import path
 
 urlpatterns = [
     path('', views.bag, name='bag'),
+    path('add/<event_id>', views.add_to_bag, name='add-to-bag'),
 ]
--- bag/views.py
+++ bag/views.py
@@ -1,7 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 
+# Modified from Code Institute Boutique Ado Walkthrough
 def bag(request):
     """A view to return the shopping bag"""
 
     return render(request, 'bag/bag.html')
+
+
+# Modified from Code Institute Boutique Ado Walkthrough
+def add_to_bag(request, event_id):
+    """ Add an Event to the shopping bag """
+
+    quantity = 1
+    redirect_url = request.POST.get('redirect_url')
+    bag = request.session.get('bag', {})
+
+    if event_id in list(bag.keys()):
+        bag[event_id] += quantity
+    else:
+        bag[event_id] = quantity
+
+    request.session['bag'] = bag
+    print(request.session['bag'])
+    return redirect(redirect_url)
