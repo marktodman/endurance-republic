3e1e77288ec9981848b65e4f713b5a5fa2a5de75
marktodman <drmtodman@gmail.com>
2023-01-23 18:09:02 +0000

    feat: Add checkout template and style

--- endurance_republic/urls.py
+++ endurance_republic/urls.py
@@ -12,5 +12,6 @@ urlpatterns = [
     path('events/', include('events.urls')),
     path('contact/', include('contact.urls')),
     path('bag/', include('bag.urls')),
+    path('checkout/', include('checkout.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 handler404 = 'endurance_republic.views.error_404'
