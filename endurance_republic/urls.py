aee69503f80666c26c8069a2b71dc086be7a8cc1
marktodman <drmtodman@gmail.com>
2023-01-27 18:15:21 +0000

    feat: Add profile template

--- endurance_republic/urls.py
+++ endurance_republic/urls.py
@@ -13,5 +13,6 @@ urlpatterns = [
     path('contact/', include('contact.urls')),
     path('bag/', include('bag.urls')),
     path('checkout/', include('checkout.urls')),
+    path('profile/', include('profiles.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 handler404 = 'endurance_republic.views.error_404'
