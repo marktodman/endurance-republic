57152c02879014c825bd19fde2724f202860041e
marktodman <drmtodman@gmail.com>
2023-01-28 16:30:35 +0000

    feat: Change add to bag view so that only authenticated users can add to bag

--- bag/views.py
+++ bag/views.py
@@ -9,27 +9,33 @@ def bag(request):
     return render(request, 'bag/bag.html')
 
 
-# Modified from Code Institute Boutique Ado Walkthrough
 def add_to_bag(request, event_id):
-    """ Add an Event to the shopping bag """
+    """ Authenticated user can add an Event to the shopping bag """
 
-    quantity = 1
-    redirect_url = request.POST.get('redirect_url')
-    bag = request.session.get('bag', {})
+    if request.user.is_authenticated:
+        quantity = 1
+        redirect_url = request.POST.get('redirect_url')
+        bag = request.session.get('bag', {})
 
-    if event_id in list(bag.keys()):
-        messages.success(request, (
-            'Sorry, you cannot book more than one place \
-                on each event! Contact us and we will help arrange your \
-                    booking.'))
+        if event_id in list(bag.keys()):
+            messages.success(request, (
+                'Sorry, you cannot book more than one place \
+                    on each event! Contact us and we will help arrange your \
+                        booking.'))
+        else:
+            bag[event_id] = quantity
+            messages.success(request, (
+                        'Success! Your event has been added to your cart'))
+
+        request.session['bag'] = bag
+
+        return redirect(redirect_url)
+
+    # For non-authenticated users trying to access the page
     else:
-        bag[event_id] = quantity
         messages.success(request, (
-                    'Success! Your event has been added to your cart'))
-
-    request.session['bag'] = bag
-
-    return redirect(redirect_url)
+            'You must be signed in to book an event.'))
+        return redirect('account_login')
 
 
 # Modified from Code Institute Boutique Ado Walkthrough
