80bbc6f7c0a4ee39f7935c33df7ebe3b8cd190d8
marktodman <drmtodman@gmail.com>
2023-01-28 13:44:37 +0000

    fix: Remove allauth custom sign up fields

--- endurance_republic/settings.py
+++ endurance_republic/settings.py
@@ -112,7 +112,6 @@ ACCOUNT_SIGNUP_EMAIL_ENTER_TWICE = True
 ACCOUNT_USERNAME_MIN_LENGTH = 4
 LOGIN_URL = '/accounts/login/'
 LOGIN_REDIRECT_URL = '/'
-ACCOUNT_FORMS = {'signup': 'endurance_republic.forms.CustomSignupForm', }
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
