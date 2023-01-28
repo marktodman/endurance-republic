9e6913c582c41b7eeb1f014fb724b3526bf1d81b
marktodman <drmtodman@gmail.com>
2023-01-28 15:26:35 +0000

    style: Style placeholder text on profile form

--- profiles/models.py
+++ profiles/models.py
@@ -15,13 +15,13 @@ class UserProfile(models.Model):
     """
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     default_phone_number = models.CharField(max_length=20, null=True, blank=True)
-    default_country = CountryField(blank_label='Country *', null=True, blank=True)
-    default_postcode = models.CharField(max_length=20, null=True, blank=True)
-    default_town_or_city = models.CharField(max_length=40, null=True, blank=True)
     default_street_address1 = models.CharField(max_length=80, null=True, blank=True)
     default_street_address2 = models.CharField(max_length=80, null=True, blank=True)
+    default_town_or_city = models.CharField(max_length=40, null=True, blank=True)
     default_county = models.CharField(max_length=80, null=True, blank=True)
-    deafult_emergency_contact = models.CharField(
+    default_postcode = models.CharField(max_length=20, null=True, blank=True)
+    default_country = CountryField(blank_label='Country', null=True, blank=True)
+    default_emergency_contact = models.CharField(
         max_length=50, null=True, blank=True)
     default_emergency_number = PhoneNumberField(null=True, blank=True)
     default_medical = models.TextField(null=True, blank=True)
